36bc02247cc1a4d398be4ee614a4a8947e57fa9f
radarpok <vdiana.m8@gmail.com>
2021-02-24 22:41:53 +0100

    updating horticulture datas for learning in jpg and ods format

---
+++ rendszertan_teli_novenyismeret_dia_2020_12_09.js
@@ -0,0 +1,3 @@
+const noveny_rendek_images = {
+        url: "https://mega.nz/folder/9mB1AaxC#kbx430ixT8EEXaQEkfm8wg"
+}
--- rendszertan_teli_novenyismereti_beszamolo_2019_2020_dia_2020_12_09.js
+++
@@ -1,3 +0,0 @@
-const noveny_rendek_images = {
-        url: "https://mega.nz/folder/8i5XyYpD#rjctoDYliE40j-70wYhh7w"
-}
