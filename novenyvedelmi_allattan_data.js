0d59eb63bf87b49cd3bd7326383ff9fc601dbfde
radarpok <vdiana.m8@gmail.com>
2021-03-02 21:02:44 +0100

    adding zoology datas for plant protection

---
+++ novenyvedelmi_allattan_data.js
@@ -0,0 +1,40 @@
+export const novenyvedelmi_allattan = [
+
+    // Arthropoda / Insecta / Hemiptera / Sterrnorhyncha / Psyllidae
+    
+    [{
+        jellemzes: [
+            'epimorfózis',
+            'kisméretűek (2-5 mm)',
+            'test felett háztetőszerűen álló, két pár hártyás szárny',
+            'erős ugrólábak',
+            'szúró-szívó szájszerv (fitofágok)',
+            'változatos kártételi módok (deformáció, mézharmat, korompenész, vektor szerep)',
+            'lárvaként gyakori a gazdanövény-specializáció'
+        ],
+
+        id: 1,
+        latin_nev: `Cacopsylla pyri`,
+        magyar_nev: `Füstösszárnyú körtelevélbolha`,
+        csalad: `Psyllidae`,
+        leiras: [
+            ''
+        ],
+        vedekezesi_lehetosegek: [
+            'megfelelő víz- és tápanyagellátás',
+            'metszés időzítése és megfelelő koronaalakítás',
+            'kémiai védekezés (időben történő észlelés, szignalizáció, permetezések időzítése, okszerű hatóanyag-választás, szerrotáció, rezisztencia elkerülése)',
+            'természetes ellenségek kímélése'
+        ]
+    }],
+    {
+        id: 2,
+        latin_nev: ``,
+        magyar_nev: ``,
+        csalad: ``,
+        leiras: [
+            ''
+        ],
+        vedekezesi_lehetosegek: []
+    }
+]
